df2214379e71f3ac611c452f36c8123221bbfe2c
Inessa Babenko <iness.tt.eln@gmail.com>
2022-05-04 00:59:06 +0700

    feat: add  main keyboard layout

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const divMainFrame = document.createElement('div');
+divMainFrame.className = 'main-frame';
+document.body.append(divMainFrame);
+
+const divKeyboardLine1 = document.createElement('div');
+divKeyboardLine1.className = 'line-1';
+divMainFrame.append(divKeyboardLine1);
+
+const divKeyboardLine2 = document.createElement('div');
+divKeyboardLine2.className = 'line-2';
+divMainFrame.append(divKeyboardLine2);
+
+const divKeyboardLine3 = document.createElement('div');
+divKeyboardLine3.className = 'line-3';
+divMainFrame.append(divKeyboardLine3);
+
+const divKeyboardLine4 = document.createElement('div');
+divKeyboardLine4.className = 'line-4';
+divMainFrame.append(divKeyboardLine4);
+
+const divKeyboardLine5 = document.createElement('div');
+divKeyboardLine5.className = 'line-5';
+divMainFrame.append(divKeyboardLine5);
+
